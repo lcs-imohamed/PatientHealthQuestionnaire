3bd1a48abed182009192b4c991a93a7c7f83d0d3
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-28 11:28:14 -0500

    Initial Commit

---
+++ Shared/PatientHealthQuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatientHealthQuestionnaireApp.swift
+//  Shared
+//
+//  Created by Mohamed, Ilana on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct PatientHealthQuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
