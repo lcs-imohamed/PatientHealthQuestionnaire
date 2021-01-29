92c9f68ff7f3f763ce3662b1344086566141b3a1
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-01-29 14:28:32 -0500

    Made a couple of formatting changes to the views

--- Shared/Views/GAD7.swift
+++ Shared/Views/GAD7.swift
@@ -13,28 +13,47 @@ struct GAD7: View {
         VStack {
             //Adds prompt to screen
             Text("Over the last two weeks, how often have you been bothered by the following problems?")
+                .font(.subheadline)
+            
+            
             Spacer()
             
             
             Text("Feeling nervous, anxious, or on edge")
+        
+            
             
             Spacer()
             
             //Adds a button for each option
             VStack{
                 Button("Not at all") {
+                    
+                    //Print function shows what will eventually happen, computer will keep track of score and present it at the end with results.
+                    
                     print("Add 0 to score")
                 }
                 
+                .padding()
+            
                 Button("Several days") {
                     print("Add 1 to score")
                 }
+                
+                .padding()
+                
                 Button("More than half of the days") {
                     print("Add 2 to score")
                 }
+                
+                .padding()
+                
+                
                 Button("Nearly everyday") {
                     print("Add 3 to score")
                 }
+                
+                .padding()
                 
                 
                 
--- Shared/Views/PHQ9.swift
+++ Shared/Views/PHQ9.swift
@@ -11,25 +11,44 @@ struct PHQ9: View {
     var body: some View {
         
         VStack {
-            Text("1. Little interest or pleasure in doing things")
+            //Adds prompt to screen
+            Text("Over the last 2 weeks, how often have you been bothered by any of the following problems?")
+                .font(.subheadline)
+            
+            Spacer()
+            
+            Text("Little interest or pleasure in doing things")
             
             Spacer()
             
             VStack{
+                //Adds buttons for user to click
                 Button("Not at all") {
+                    
+                    //Print function shows what will eventually happen, computer will keep track of score and present it at the end with results.
+                    
                     print("Add 0 to score")
                 }
+                
+                .padding()
                 
                 Button("Several days") {
                     print("Add 1 to score")
                 }
+                
+                .padding()
+                
                 Button("More than half of the days") {
                     print("Add 2 to score")
                 }
+                
+                .padding()
+                
                 Button("Nearly everyday") {
                     print("Add 3 to score")
                 }
                 
+                .padding()
             
                 
             }
@@ -38,13 +57,14 @@ struct PHQ9: View {
         
         }
        
-        
+        //Adds title to screen
             .navigationTitle("PHQ-9")
     }
 }
 
 struct PHQ9_Previews: PreviewProvider {
     static var previews: some View {
+        //Allows user to navigate to this view
         NavigationView {
             PHQ9()
         }
