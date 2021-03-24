7bcbf16a2028bfd64a95dd4ff07d4295c3277641
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-24 10:56:03 -0400

    Created a structure response to show possible responses and keep track of the score attached to them

---
+++ Shared/Model/Response.swift
@@ -0,0 +1,14 @@
+//
+//  Response.swift
+//  PatientHealthQuestionnaire (iOS)
+//
+//  Created by Mohamed, Ilana on 2021-03-24.
+//
+
+import Foundation
+
+struct Response: Identifiable {
+    let id = UUID()
+    let responseText: String
+    let responseScore: Int
+}
