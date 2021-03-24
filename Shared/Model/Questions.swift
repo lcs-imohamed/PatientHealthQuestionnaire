dfbad9fed8387c40b7e1d02eeb21b537b8145531
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-24 12:20:28 -0400

    Created a list of questions for the PHQ-9 view

---
+++ Shared/Model/Questions.swift
@@ -0,0 +1,40 @@
+//
+//  Questions.swift
+//  PatientHealthQuestionnaire (iOS)
+//
+//  Created by Mohamed, Ilana on 2021-03-24.
+//
+
+import Foundation
+
+class Questions {
+    
+    // properties
+    var list: [Question] = []
+    
+    // Initializer
+    
+    init() {
+        
+        // Define the different questions
+        
+        list.append (Question(questionText:"Little interest or pleasure in doing things"))
+        
+        list.append(Question(questionText: "2. Feeling down, depressed, or hopeless"))
+        
+        list.append(Question(questionText: "3. Trouble falling or staying asleep, or sleeping too much"))
+        
+        list.append(Question(questionText: "4. Feeling tired or having little energy"))
+        
+        list.append(Question(questionText: "5. Poor appetite or overeating"))
+        
+        list.append(Question(questionText: "6. Feeling bad about yourself – or that you are a failure or have let yourself or your family down"))
+        
+        list.append(Question(questionText: "7. Trouble concentrating on things, such as reading the newspaper or watching television"))
+        
+        list.append(Question(questionText: "8. Moving or speaking so slowly that other people could have noticed? Or the opposite – being so fidgety or restless that you have been moving around a lot more than usual"))
+        
+        list.append(Question(questionText: "9. Thoughts that you would be better off dead or of hurting yourself in some way"))
+       
+    }
+}
