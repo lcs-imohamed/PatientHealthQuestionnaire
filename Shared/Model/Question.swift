747b793468ef191bfa84a2d20015ba50530f30f2
Ilana Mohamed <Imohamed@lcs.on.ca>
2021-03-30 12:18:04 -0400

    Added integer id field for responses

--- Shared/Model/Question.swift
+++ Shared/Model/Question.swift
@@ -21,78 +21,77 @@ struct Question: Identifiable {
 let questions = [
 
     Question(id: 1, questionText: "1. Little interest or pleasure in doing things", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     ]),
     
     Question(id: 2, questionText: "2. Feeling down, depressed, or hopeless", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     
     Question(id: 3, questionText: "3. Trouble falling or staying asleep, or sleeping too much", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     
     Question(id: 4, questionText: "4. Feeling tired or having little energy", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     Question(id: 5, questionText: "5. Poor appetite or overeating", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     Question(id: 6, questionText: "6. Feeling bad about yourself – or that you are a failure or have let yourself or your family down", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     Question(id: 7, questionText: "7. Trouble concentrating on things, such as reading the newspaper or watching television", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     
     Question(id: 8, questionText: "8. Moving or speaking so slowly that other people could have noticed? Or the opposite – being so fidgety or restless that you have been moving around a lot more than usual", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     
     ]),
     
     
     Question(id: 9,questionText: "9. Thoughts that you would be better off dead or of hurting yourself in some way", Responses: [
-        Response(responseText: "Not at all", responseScore: 0),
-        Response(responseText: "Several days", responseScore: 1),
-        Response(responseText: "More than half the days", responseScore: 2),
-        Response(responseText: "Nearly every day", responseScore: 3)
-    
+        Response(id: 1, responseText: "Not at all", responseScore: 0),
+        Response(id: 2, responseText: "Several days", responseScore: 1),
+        Response(id: 3, responseText: "More than half the days", responseScore: 2),
+        Response(id: 4, responseText: "Nearly every day", responseScore: 3)
     ]),
     
     
--- Shared/Model/Response.swift
+++ Shared/Model/Response.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Response: Identifiable {
-    let id = Int()
+    let id: Int
     let responseText: String
     let responseScore: Int
     
